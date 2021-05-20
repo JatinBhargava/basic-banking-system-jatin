e0b3b697a5072e8270a9a5c6255a9fb86f499d5b
Jatin Bhargava <jatinbhargava4@gmail.com>
2021-05-20 19:30:18 +0530

    initial commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$name = "curd";
+
+$conn = mysqli_connect($host, $user, $pass, $name);
+
+    if(!$conn){
+        die("Could not connect to the Database".mysqli_connect_error());
+    }
+
+?>
+
---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title> Basic Banking System </title>
+</head>
+
+<body>
+<!--Navigation-->   
+    <div class="nav">
+        <div class="menu">
+             <ul>
+                 <li><a href="./home.php">Home</a></li>
+                 <li><a href="./viewusers.php">Contact</a></li>
+                 <li><a href="./transactionDetails.php">History</a> </li>
+                 <li><a href="./aboutus.html">About Us</a> </li>
+             </ul>
+        </div>
+    </div> 
+<!--Navigation Ends-->
+
+<!--Middle Part-->
+<div class="center-part">
+    <div>
+        <h1 class="Heading">SF Banking</h1>
+        <p class="text">
+            A banking system is a group or network of institutions that provide financial services for us.<br>
+             Institutions are responsible for operating a payment system, providing loans, taking deposits, and 
+             <br> helping with investments
+        </p>
+        <div>
+            <img src="BBIMG.jpg" alt="Banking Img" class="img">     
+
+        </div>
+       
+    </div>
+
+</div>
+
+<!--Middle Part Ends-->
+
+<!--Footer-->
+ <div class="footer">
+     <div class="intro">
+      <p>Sparks Banking System| Jatin Bhargava c 2021</p>
+
+     </div>
+ </div>
+
+<!--Footer Ends-->
+</body>
+
+</html>
